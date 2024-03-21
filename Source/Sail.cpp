7fc3f9d7b5f9bcab15a3cd2a42380bf406d4c016
Sasan Keshmiri <sasan.keshmiri@gmail.com>
2024-03-22 00:32:25 +0100

    Changed to SDL for graphics library. Started on SDL tutorial to create window with picture and handle key press events.

--- Source/Sail.cpp
+++ Source/Sail.cpp
@@ -1,69 +1,208 @@
 // TODO 11: Include TutorialConfig.h
 //#include "SailConfig.h"
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
 
 #include <iostream>
 
-void framebuffer_size_callback(__attribute__((unused)) GLFWwindow* window, const int width, const int height)
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Key press surfaces constants
+enum KeyPressSurfaces
 {
-	glViewport(0, 0, width, height);
+	KEY_PRESS_SURFACE_DEFAULT,
+	KEY_PRESS_SURFACE_UP,
+	KEY_PRESS_SURFACE_DOWN,
+	KEY_PRESS_SURFACE_LEFT,
+	KEY_PRESS_SURFACE_RIGHT,
+	KEY_PRESS_SURFACE_TOTAL
+};
+
+SDL_Window* gWindow{NULL};
+SDL_Surface* gScreenSurface{NULL};
+SDL_Surface* gHelloWorld{NULL};
+SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+SDL_Surface* gCurrentSurface{NULL};
+
+//Starts up SDL and creates window
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("Sail", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+			success = false;
+		}
+		else
+		{
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	return success;
 }
 
-void processInput(GLFWwindow *window)
+SDL_Surface* loadSurface(const std::string path)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
+	//Load image at specified path
+	SDL_Surface* loadedSurface{SDL_LoadBMP(path.c_str())};
+	if (loadedSurface == NULL)
+	{
+		std::cout << "Unable to load image " << path << "! SDL_Error: " << SDL_GetError() << std::endl;
+	}
+
+	return loadedSurface;
 }
 
-int main()
+//Loads media
+bool loadMedia()
 {
-	if (!glfwInit())
+	//Loading success flag
+	bool success = true;
+
+	//Load default surface
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("../Source/press.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == NULL)
 	{
-		std::cout << "Failed to initialize GLFW" << std::endl;
-		return -1;
+		std::cout << "Failed to load default image!" << std::endl;
+		success = false;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Sail", NULL, NULL);
-	if (window == NULL)
+	//Load up surface
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("../Source/up.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
+		std::cout << "Failed to load up image!" << std::endl;
+		success = false;
 	}
 
-	glViewport(0, 0, 800, 600);
-
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	//render loop
-	while (!glfwWindowShouldClose(window))
+	//Load down surface
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("../Source/down.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL)
 	{
-		//input
-		processInput(window);
-
-		//rendering commands
-		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		//check and call events and swap the buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		std::cout << "Failed to load down image!" << std::endl;
+		success = false;
 	}
 
-	glfwTerminate();
+	//Load left surface
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("../Source/left.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] == NULL)
+	{
+		std::cout << "Failed to load left image!" << std::endl;
+		success = false;
+	}
+
+	//Load right surface
+	gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("../Source/right.bmp");
+	if (gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] == NULL)
+	{
+		std::cout << "Failed to load right image!" << std::endl;
+		success = false;
+	}
+
+	return success;
+}
+
+//Frees media and shuts down SDL
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char* args[])
+{
+	//Start up SDL and create window
+	if (!init())
+	{
+		std::cout << "Failed to initialize!" << std::endl;
+	}
+	else
+	{
+		//Load media
+		if (!loadMedia())
+		{
+			std::cout << "Failed to load media!" << std::endl;
+		}
+		else
+		{
+			//Main loop flag
+			bool quit{false};
+			//Event handler
+			SDL_Event e{};
+
+			//Set default current surface
+			gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+
+			//While application is running
+			while (!quit)
+			{
+				//Handle events on queue
+				while (SDL_PollEvent(&e) != 0)
+				{
+					//User requests quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+					//User presses a key
+					else if (e.type == SDL_KEYDOWN)
+					{
+						//Select surfaces based on key press
+						switch (e.key.keysym.sym)
+						{
+							case SDLK_UP:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_UP];
+								break;
+
+							case SDLK_DOWN:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+								break;
+
+							case SDLK_LEFT:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
+								break;
+
+							case SDLK_RIGHT:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
+								break;
+
+							default:
+								gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+								break;
+						}
+					}
+				}
+
+				//Apply the image
+				SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
+				SDL_UpdateWindowSurface(gWindow);
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	close();
 
   return 0;
 }
